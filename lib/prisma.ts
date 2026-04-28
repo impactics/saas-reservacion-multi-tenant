eeac37267d81bfedb5d5cbbac4d6a8db61c93aca
Impactics <impacticsec@gmail.com>
2026-04-27 22:32:42 -0500

    fix: add default export to prisma client (fixes "Export default doesn't exist")

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -21,3 +21,5 @@ function createPrismaClient() {
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
